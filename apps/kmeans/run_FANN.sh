b2d43d27d484beb095d25d612431c11cc81ab055
Amir Yazdanbakhsh <amir.yazdanbakhsh@gmail.com>
2014-10-29 00:52:29 -0400

    sobel and kmeans are done

--- apps/kmeans/run_FANN.sh
+++ apps/kmeans/run_FANN.sh
@@ -9,5 +9,5 @@ make NPU_MODE=NPU_FANN
 ./bin/${BENCHMARK}_NPU_FANN.out ./data/peppers-512px.rgb ./cfg/${BENCHMARK}_FANN.nn
 cd data
 python png2rgb.py png peppers-512px.rgb_fann.rgb peppers-512px_FANN.png
-python png2rgb.py png peppers-512px.rgb peppers-512px_orig.png
+python png2rgb.py png peppers-512px.rgb_orig.rgb peppers-512px_orig.png
 compare -metric RMSE peppers-512px_FANN.png peppers-512px_orig.png null
