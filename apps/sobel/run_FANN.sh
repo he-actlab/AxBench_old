61f74bbc104f1c45ba2d03022493df6dc65db873
Amir Yazdanbakhsh <amir.yazdanbakhsh@gmail.com>
2014-10-30 23:26:54 -0400

    sobel error

--- apps/sobel/run_FANN.sh
+++ apps/sobel/run_FANN.sh
@@ -16,7 +16,7 @@ python png2rgb.py png ${IMAGE}.rgb_fann.rgb ${IMAGE}.rgb_fann.png > out2.tmp
 compare -metric RMSE ${IMAGE}.rgb_orig.png ${IMAGE}.rgb_fann.png null > tmp.log 2> tmp.err
 echo -en "\033[31m"
 echo "--------------------"
-awk '{ 	printf("Error: %0.2f%\n",substr($2, 2, length($2) - 2) * 100) }' tmp.err
+awk '{ 	printf("Sobel Error: %0.2f%\n",substr($2, 2, length($2) - 2) * 100) }' tmp.err
 echo "--------------------"
 echo -en "\033[0m"
 echo ""
