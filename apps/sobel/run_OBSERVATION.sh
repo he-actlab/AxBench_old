5338865a4ebb672a5198015a5c1c0b1227ede8b9
Amir Yazdanbakhsh <amir.yazdanbakhsh@gmail.com>
2014-10-30 21:44:48 -0400

    sobel in observation mode is added

---
+++ apps/sobel/run_OBSERVATION.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BENCHMARK=sobel
+
+# Run the program
+echo -e "# Make the $BENCHMARK for running in observation mode...\n"
+make NPU_MODE=NPU_OBSERVATION clean
+make NPU_MODE=NPU_OBSERVATION
+IMAGE=peppers
+echo -en "\033[36m"
+./bin/${BENCHMARK}_NPU_OBSERVATION.out ./data/${IMAGE}.rgb ./data/${IMAGE}.data
+echo -en "\033[0m"
+cd data
+python png2rgb.py png observation.rgb ${IMAGE}.observation.png > /tmp/out1
+echo ""
+echo "Thank you for using ** npu.bench **..."
+echo ""
