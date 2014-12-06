00abcdfef3ea5d25dd30e821955a639bdbf3cfbc
Amir Yazdanbakhsh <a.yazdanbakhsh@gatech.edu>
2014-12-05 21:30:27 -0500

    Brent-Kung 32-bit adder is added

---
+++ hardware/circuits/brent_kung_32b/scripts/simulate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+red='\033[0;31m'
+blue='\033[0;34m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+nc='\033[0m' # No Color
+
+echo -e "\n${blue}******************************************"
+echo -e "*            AxBench Hardware            *"
+echo -e "******************************************${nc}"
+
+echo -e "${green} # Simulating 32-bit Brent-Kung Adder...${nc}"
+
+ncverilog ../testbench/brent_kung_32b_tb.v > ../log/brent_kung_32b_tb.log
+
+echo -e "${blue}Thanks you...${nc}\n"
